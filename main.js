355e85baacbd731c44e021e08c6d45b1ec562705
rsvincent <rvincent87@gmail.com>
2023-11-14 17:17:48 -0500

    audio from last song when play is clicked

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const url = "https://itunes.apple.com/search?limit=10&media=music&term=";
+const url = "https://itunes.apple.com/search?limit=15&media=music&term=";
 let userInput = document.getElementById("userInput");
 let save = document.getElementById("submit");
 let inputField = document.getElementById("inputField");
@@ -14,6 +14,7 @@ audioPlay.controls = true;
 audioPlayers.appendChild(audioPlay)
 
 // })
+
 userInput.addEventListener("click", (event) => {
     event.preventDefault();
     let term = inputField.value;
@@ -26,31 +27,30 @@ userInput.addEventListener("click", (event) => {
                 // console.log(result);
 
                 artistResult.innerHTML += `
-                    <div class="containter" id="content" data-id="selectedSong">
+               
+                    <div class="audioContainter" id="content" data-id="song">
 <div class="artist">${result.artistName} </div>
 <div class= "track">${result.trackName}</div>
-<img id="player" src= ${result.artworkUrl100.replace("100x100", "380x380")}>
+<img id="player" src= ${result.artworkUrl100.replace("100x100", "280x280")}></div>
+<button class="playBack" id="playSong">PLAY</button> 
 </div>
 `;
-                //currently only plays the 1st returned result when clicked 
-                const audio = document.getElementById("content");
-                console.log(audio)
+                const audio = document.getElementById("playSong");
+                audio.addEventListener('click', function (event) {
 
-                audio.addEventListener("click", () => {
-                    audioPlay.src = result.previewUrl;
-                });
-                // const playSong = document.getElementById("selectedSong");
-                // function addEvent() {
-                //     playSong.addEventListener("click", play)
-                //     //souce placement here will play the 1st track from search 
-                // }
-                // function play() {
-                //     audioPlay.src = result.previewUrl;
-                //     play();
-                // }
-                // });
+                    let target = event.target;
 
+                    songSelected(target)
+
+                    function songSelected() {
+                        audioPlay.src = result.previewUrl;
+                    }
+
+                })
             });
+
+
+
         });
 
 
